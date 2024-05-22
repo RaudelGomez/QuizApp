55bde5cca7b226e9e5c681edd91ec9f70375a70f
Raudel Gomez Smith <gomezsmith.raudel@gmail.com>
2024-05-22 15:02:34 +0200

    end version

--- script.js
+++ script.js
@@ -57,4 +57,154 @@ let questions = [
 	},
 ];
 
-function init() {}
+let currentQuestion = 0;
+let numberRightAnswer = 0;
+let audio_success = new Audio("./audio/success.wav");
+let audio_fail = new Audio("./audio/wrong.mp3");
+let audio_end = new Audio("./audio/goodbye-honey.wav");
+
+function init() {
+	audio_end.pause();
+	audio_end.currentTime = 0;
+	document.getElementById("total-questions").innerHTML = questions.length;
+	showQuestion();
+}
+
+function showQuestion() {
+	if (gameisOver()) {
+		//End Screen
+		showEndScreen();
+	} else {
+		// Show question
+		updateProgressBar();
+		updateNextQuestion();
+	}
+}
+
+function gameisOver() {
+	return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+	document.getElementById("endScreen").style = "";
+	document.getElementById("questionsBody").style = "display: none";
+	document.getElementById("total-questions-result").innerHTML =
+		questions.length;
+	document.getElementById("right-answers-number").innerHTML = numberRightAnswer;
+	document.getElementById("header-img").src = "./img/trophy.png";
+	audio_end.play();
+}
+
+function updateProgressBar() {
+	let porcent = (currentQuestion + 1) / questions.length;
+	porcent = Math.round(porcent * 100);
+	document.getElementById("progress-bar").innerHTML = `${porcent}%`;
+	document.getElementById("progress-bar").style = `width: ${porcent}%`;
+}
+
+function updateNextQuestion() {
+	let question = questions[currentQuestion];
+	document.getElementById("number-current-question").innerHTML =
+		currentQuestion + 1;
+	document.getElementById("questionText").innerHTML = question.question;
+	document.getElementById("answer_1").innerHTML = question.answer1;
+	document.getElementById("answer_2").innerHTML = question.answer2;
+	document.getElementById("answer_3").innerHTML = question.answer3;
+	document.getElementById("answer_4").innerHTML = question.answer4;
+}
+
+function answer(currentAnswer) {
+	let question = questions[currentQuestion];
+	let selectQuestionNumber = +currentAnswer.slice(-1);
+	let idRightAnswer = `answer_${question.right_answer}`;
+	if (rightAnswerSelected(question, selectQuestionNumber)) {
+		wellDone(selectQuestionNumber);
+		optionDisabled();
+	} else {
+		failSorry(selectQuestionNumber, idRightAnswer);
+		optionDisabled();
+	}
+	document.getElementById("nextButton").disabled = false;
+}
+
+function rightAnswerSelected(question, selectQuestionNumber) {
+	return question.right_answer === selectQuestionNumber;
+}
+
+function nextQuestion() {
+	currentQuestion++;
+	document.getElementById("nextButton").disabled = true;
+	resetAnswer();
+	showQuestion();
+	optionEnable();
+}
+
+function wellDone(selectQuestionNumber) {
+	document
+		.getElementById(`answer_${selectQuestionNumber}`)
+		.parentNode.classList.add("bg-success");
+	numberRightAnswer++;
+	audio_success.play();
+}
+
+function failSorry(selectQuestionNumber, idRightAnswer) {
+	document
+		.getElementById(`answer_${selectQuestionNumber}`)
+		.parentNode.classList.add("bg-danger");
+	document.getElementById(idRightAnswer).parentNode.classList.add("bg-success");
+	audio_fail.play();
+}
+
+function resetAnswer() {
+	document
+		.getElementById("answer_1")
+		.parentNode.classList.remove("bg-danger", "bg-success");
+	document
+		.getElementById("answer_2")
+		.parentNode.classList.remove("bg-danger", "bg-success");
+	document
+		.getElementById("answer_3")
+		.parentNode.classList.remove("bg-danger", "bg-success");
+	document
+		.getElementById("answer_4")
+		.parentNode.classList.remove("bg-danger", "bg-success");
+}
+
+function restartGame() {
+	document.getElementById("header-img").src = "./img/quiz.jpg";
+	currentQuestion = 0;
+	numberRightAnswer = 0;
+	document.getElementById("endScreen").style = "display: none";
+	document.getElementById("questionsBody").style = "";
+	init();
+}
+
+function optionDisabled() {
+	document
+		.getElementById("answer_1")
+		.parentNode.classList.add("cursor-disabled");
+	document
+		.getElementById("answer_2")
+		.parentNode.classList.add("cursor-disabled");
+	document
+		.getElementById("answer_3")
+		.parentNode.classList.add("cursor-disabled");
+	document
+		.getElementById("answer_4")
+		.parentNode.classList.add("cursor-disabled");
+}
+
+function optionEnable() {
+	document
+		.getElementById("answer_1")
+		.parentNode.classList.remove("cursor-disabled");
+	document
+		.getElementById("answer_2")
+		.parentNode.classList.remove("cursor-disabled");
+	document
+		.getElementById("answer_3")
+		.parentNode.classList.remove("cursor-disabled");
+	document
+		.getElementById("answer_4")
+		.parentNode.classList.remove("cursor-disabled");
+}
